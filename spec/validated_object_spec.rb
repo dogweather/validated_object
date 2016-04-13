10480cce0e54da3e7ba882c9c3e59700cac66020
Robb Shecter <robb@weblaws.org>
2016-04-12 18:51:34 -0700

    Failing spec for Boolean

--- spec/validated_object_spec.rb
+++ spec/validated_object_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'spec_helper'
 
 describe ValidatedObject do
@@ -8,35 +9,47 @@ describe ValidatedObject do
   it 'can be referenced' do
     expect(ValidatedObject::Base).not_to be nil
   end
-  
-  it 'verifies a valid type' do
-    class Apple1 < ValidatedObject::Base
-      attr_accessor :diameter
-      validates :diameter, type: Float
-    end
-    
-    small_apple = Apple1.new { |a| a.diameter = 2.0  }
-    expect( small_apple ).to be_valid
-  end
 
-  it 'rejects an invalid type' do
-    class Apple2 < ValidatedObject::Base
-      attr_accessor :diameter
-      validates :diameter, type: Float
-    end
-    
-    expect {
-      small_apple = Apple2.new { |a| a.diameter = '2'  }
-    }.to raise_error(ArgumentError)
-  end
+  context 'TypeValidator' do
+    it 'verifies a valid type' do
+      class Apple1 < ValidatedObject::Base
+        attr_accessor :diameter
+        validates :diameter, type: Float
+      end
 
-  it 'can verify a subclass' do
-    class Apple3 < ValidatedObject::Base
-      attr_accessor :diameter
-      validates :diameter, type: Numeric
+      small_apple = Apple1.new { |a| a.diameter = 2.0 }
+      expect( small_apple ).to be_valid
     end
-    
-    small_apple = Apple3.new { |a| a.diameter = 5  }
-    expect( small_apple ).to be_valid
+
+    it 'rejects an invalid type' do
+      class Apple2 < ValidatedObject::Base
+        attr_accessor :diameter
+        validates :diameter, type: Float
+      end
+
+      expect {
+        Apple2.new { |a| a.diameter = '2' }
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'can verify a subclass' do
+      class Apple3 < ValidatedObject::Base
+        attr_accessor :diameter
+        validates :diameter, type: Numeric
+      end
+
+      small_apple = Apple3.new { |a| a.diameter = 5 }
+      expect( small_apple ).to be_valid
+    end
+
+    it 'handles Boolean types' do
+      class Apple4 < ValidatedObject::Base
+        attr_accessor :rotten
+        validates :diameter, type: Boolean
+      end
+
+      small_apple = Apple3.new { |a| a.rotten = true }
+      expect( small_apple ).to be_valid
+    end
   end
 end
