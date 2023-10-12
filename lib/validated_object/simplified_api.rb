77c33f9c1de4ea78d4e9da65f46256aaa15018fd
Robb Shecter <robb@public.law>
2023-10-12 13:27:16 -0600

    doc

--- lib/validated_object/simplified_api.rb
+++ lib/validated_object/simplified_api.rb
@@ -1,10 +1,13 @@
 require "active_support/concern"
 
+# Enable a simplified API for the common case of
+# read-only ValidatedObjects.
 module ValidatedObject
   module SimplifiedApi
     extend ActiveSupport::Concern
 
     class_methods do
+      # Simply delegate to `attr_reader` and `validates`.
       def validated_attr(attribute, *options)
         attr_reader attribute
         validates attribute, *options
